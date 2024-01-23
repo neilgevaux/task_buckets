1aaa475fa0d068ed60eaee41bb3e6ae7cc048043
Neil Gevaux <neilgevaux@gmail.com>
2024-01-23 21:13:40 +0000

    Add param validators

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,26 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description)
+    end
 end
